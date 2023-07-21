04348ea234eda2906480a7e5f63e99685156bfb4
AndresC1 <andrescornejolira@gmail.com>
2023-07-20 22:50:51 -0600

    Integracion de tailwind, react-icons y react-route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter, } from 'react-router-dom'
+import Home from './pages/Home/Home.tsx'
+import Layout from './pages/Layout/Layout.tsx'
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </React.StrictMode>,
 )
