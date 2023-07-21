f106764bff245620fa67decd7ca8c40973f85b08
AndresC1 <andrescornejolira@gmail.com>
2023-07-21 13:03:17 -0600

    Creacion del pie de pagina

---
+++ src/pages/Layout/Footer/contract/Contract.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contract() {
+  return (
+    <section className='uppercase font-semibold flex justify-start gap-4 text-sm'>
+        <a href="">term & condition</a>
+        <a href="">policy</a>
+        <a href="">map</a>
+    </section>
+  )
+}
--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
+import Footer from './Footer/Footer'
 
 export default function Layout() {
   return (
-    <>
-        <Navbar />
-        <Outlet />
-    </>
+    <main className='flex justify-between flex-col h-auto min-h-[100vh]'>
+        <div>
+          <Navbar />
+          <Outlet />
+        </div>
+        <Footer/>
+    </main>
   )
 }
