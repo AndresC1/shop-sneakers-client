e83eed198cee6d819ab956458665ce8d907fcf4c
AndresC1 <andrescornejolira@gmail.com>
2023-07-21 13:16:52 -0600

    pie de pagina responsive

--- src/pages/Layout/Footer/social/Social.tsx
+++ src/pages/Layout/Footer/social/Social.tsx
@@ -4,7 +4,7 @@ import { BiLogoFacebook } from 'react-icons/bi';
 
 export default function Social() {
   return (
-    <section className='flex justify-start items-center gap-4'>
+    <section className='flex justify-start flex-col md:flex-row items-center gap-4'>
         <h1>follow us on social</h1>
         <span className='flex justify-start items-center gap-2'>
             <a href=""><AiOutlineInstagram className="w-5 h-auto" /></a>
